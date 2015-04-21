2acfeccd40949066d890c01629a374fbd703b079
Jordan Lev <jordan@webconcentrate.com>
2015-04-21 17:28:21 -0500

    notification email now includes html body as well (with form fields laid out in a table so they line up nicely)

--- packages/custom_contact_form/mail/admin_notify.php
+++ packages/custom_contact_form/mail/admin_notify.php
@@ -1,9 +1,14 @@
 <?php defined('C5_EXECUTE') or die("Access Denied.");
+$th = Loader::helper('text');
 
 $subject = 'New Website Form Submission: ' . $form_title;
 
 $body = 'Submitted on ' . date('n/j/y \a\t g:ia', $timestamp) . ' from page "' . $page_title . '":' . "\n\n";
+$bodyHTML = '<p>' . $th->entities($body) . '</p><table><tbody>' . "\n";
+
 foreach ($fields as $name => $field) {
 	$body .= "{$field['label']}: {$field['value']}\n";
+	$bodyHTML .= '<tr><td align="right">' . $th->entities($field['label']) . ':</td><td>' . $th->entities($field['value']) . '</td></tr>'. "\n";
 }
 $body .= "\n";
+$bodyHTML .= "</tbody></table>\n";
